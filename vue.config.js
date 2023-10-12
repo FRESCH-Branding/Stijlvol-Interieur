b40ef3dabb31e3052ddf2aa00dfc5dd7b4eefcbf
Freek van den Bosch <103334889+FRESCH-Branding@users.noreply.github.com>
2023-10-12 20:19:34 +0200

    sitemap update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,44 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+// You can write the paths as an array of strings or an array of objects
+const paths = [
+  {
+      path: '/',
+      lastmod: '2023-10-12',
+      priority: 1.0,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/over/',
+      lastmod: '2023-10-12',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/projecten/',
+      lastmod: '2023-10-12',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/contact/',
+      lastmod: '2023-10-12',
+      priority: 0.9,
+      changefreq: 'yearly'
+  }
+]
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new SitemapPlugin({ base: 'https://stijlvolinterieur.nl/', paths })
+        ]
+    },
+    // Other exports here
+}
+
 
 // const { defineConfig } = require('@vue/cli-service');
 // module.exports = defineConfig({
